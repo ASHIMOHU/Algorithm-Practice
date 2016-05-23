30dd6df47f98e5b356dfa96c6307ed4268e8a8b6
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-05-23 15:09:25 +0900

    a

--- Euclid.java
+++ Euclid.java
@@ -4,16 +4,17 @@ public class Euclid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt(), y = sc.nextInt(), r = division(x, y);
-        while (r != 0){
+        while (r != 0) {
             x = y;
             y = r;
             r = division(x, y);
-            if (r == 0){
+            if (r == 0) {
                 System.out.println(y + "が最大公約数です。");
             }
         }
     }
-    private static int division(int x, int y) {
+
+    public static int division(int x, int y) {
         int z = x % y;
         System.out.println(String.format("%d と %d を割った結果、余り %d が出力されました", x, y, z));
         return z;
